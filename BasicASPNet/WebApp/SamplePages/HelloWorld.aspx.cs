52089f505a37cbd076cb3683ab53de0005f4cd70
fshoaib1 <48365666+fshoaib1@users.noreply.github.com>
2019-10-15 13:14:17 -0600

    Create new asp.net app
    investigate asp.net webapp and create helloworld page

---
+++ BasicASPNet/WebApp/SamplePages/HelloWorld.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApp.SamplePages
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            HelloText.Text = "Darn It !";
+            HelloText.Font.Size = 100;
+            HelloText.ForeColor = System.Drawing.Color.Crimson;
+        }
+    }
+}
